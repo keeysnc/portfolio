ba5d579cdd37abb8b033f0da3a23961a1244eab3
Nathan Keeyss <keeysnc@gmail.com>
2020-07-21 17:51:21 -0400

    Updates

---
+++ precache-manifest.0e209ec9eaf75e06d0d5d656ccd37faa.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ccd47352031b79f380f1dd2ce094af",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1df79454faf43fe39885",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9e03eedcdc81d24c4873",
+    "url": "/portfolio/static/css/main.d656ba0c.chunk.css"
+  },
+  {
+    "revision": "1df79454faf43fe39885",
+    "url": "/portfolio/static/js/2.404896b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.404896b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e03eedcdc81d24c4873",
+    "url": "/portfolio/static/js/main.e493ca80.chunk.js"
+  },
+  {
+    "revision": "b37b5bb5a98282a2f0e7",
+    "url": "/portfolio/static/js/runtime-main.b449097c.js"
+  },
+  {
+    "revision": "4d7619a1fcaf462726fad7e8870eb899",
+    "url": "/portfolio/static/media/80percent-logo.4d7619a1.svg"
+  },
+  {
+    "revision": "1eca0443adaa36a64deb443d54de7bfd",
+    "url": "/portfolio/static/media/about-logo-alt.1eca0443.svg"
+  },
+  {
+    "revision": "f7b7165a20291612cc4c08bcb385df24",
+    "url": "/portfolio/static/media/about-logo.f7b7165a.svg"
+  },
+  {
+    "revision": "45f740898953090e27b6b1beffcf8988",
+    "url": "/portfolio/static/media/about-pic.45f74089.png"
+  },
+  {
+    "revision": "9e77165b2885bd7c27c686a19f53078c",
+    "url": "/portfolio/static/media/ads-hero.9e77165b.jpg"
+  },
+  {
+    "revision": "d8801cbf0c6f73efe35315817d4c4893",
+    "url": "/portfolio/static/media/aqi-gif.d8801cbf.gif"
+  },
+  {
+    "revision": "89a33a20553471a2f1c4a9695f1bdf52",
+    "url": "/portfolio/static/media/aqi-work-hero.89a33a20.jpg"
+  },
+  {
+    "revision": "fa51026cc879f9069a004f73f1f22e66",
+    "url": "/portfolio/static/media/codepen-alt.fa51026c.svg"
+  },
+  {
+    "revision": "467e3b36734d849e9484916ecb9463ea",
+    "url": "/portfolio/static/media/codepen.467e3b36.svg"
+  },
+  {
+    "revision": "6d57d64a1d1df0d923e1b9a63c93ca88",
+    "url": "/portfolio/static/media/home.6d57d64a.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "af658bed1c25869e6c7ba159e12659df",
+    "url": "/portfolio/static/media/medium.af658bed.svg"
+  },
+  {
+    "revision": "c62c4099869c7bbe273a81dc38eb969e",
+    "url": "/portfolio/static/media/ns-hero.c62c4099.jpg"
+  },
+  {
+    "revision": "d852e7e235ad2e46fc8f44aacbb0af11",
+    "url": "/portfolio/static/media/resume-alt.d852e7e2.svg"
+  },
+  {
+    "revision": "d939a369331c806e7f1024fefdd648d3",
+    "url": "/portfolio/static/media/resume.d939a369.svg"
+  },
+  {
+    "revision": "4dff2e69b7a6eba534c81e7c58e51b63",
+    "url": "/portfolio/static/media/rp3agency.4dff2e69.jpg"
+  }
+]);

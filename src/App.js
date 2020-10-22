692e4049b609c7ddb18d51d72a667060076c1557
Nathan Keeyss <keeysnc@gmail.com>
2020-10-21 22:33:00 -0400

    add basename to router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './index'
 import Bio from './components/Bio';
 import About from './components/About';
 import Work from './components/Work';
@@ -17,25 +16,25 @@ import resumeLogo from './img/resume.svg'
 import homeLogo from './img/home.svg'
 import aboutLogo from './img/about-logo.svg'
 import musicLogo from './img/80percent-logo.svg'
-// import {
-//   HashRouter as Router,
-//   Switch,
-//   Route,
-//   Link
-// } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 function App() {
 
   return (
-    // <Router basename={window.location.pathname || ''}>
+    <Router basename ="/portfolio">
       <div className="App">
         <header className="App-header">
           <Container>
             <ul>
-              {/* <Link alt="home" to="/"><img className="homeIcon" src={homeLogo}></img></Link> */}
+              <Link alt="home" to="/"><img className="homeIcon" src={homeLogo}></img></Link>
               <div className="pull-right">
-                {/* <Link alt="about" to="/about"><img className="aboutIcon" src={aboutLogo}></img></Link> */}
+                <Link alt="about" to="/about"><img className="aboutIcon" src={aboutLogo}></img></Link>
                 <a alt="codepen" target="_blank" href="https://codepen.io/lwdstudio"><img className="codepenIcon icon" src={codePenLogo}></img></a>
                 <a alt="medium" target="_blank" href="https://medium.com/@likewaterdesignco"><img className="mediumIcon icon" src={mediumLogo}></img></a>
                 <a alt="resume" target="_blank" href={process.env.PUBLIC_URL + '/resume.pdf'}><img className="resumeIcon icon" src={resumeLogo}></img></a>
@@ -46,32 +45,32 @@ function App() {
         </header>
 
         <Container>
-          {/* <Switch> */}
-            {/* <Route exact path="/"/> */}
+          <Switch>
+            <Route exact path="/">
               <Bio/>
-            {/* <Route /> */}
-          {/* </Switch>
-          <Switch> */}
-            {/* <Route exact path="/about"> */}
+            </Route>
+          </Switch>
+          <Switch>
+            <Route exact path="/about">
               <About/>
-            {/* </Route>
-          </Switch> */}
+            </Route>
+          </Switch>
         </Container>
-            {/* <Route exact path="/work/aqi-visualizer"> */}
+            <Route exact path="/work/aqi-visualizer">
               <Work hero={aqiHero} />
-            {/* </Route> */}
-            {/* <Route exact path="/work/rp3agency"> */}
+            </Route>
+            <Route exact path="/work/rp3agency">
               <Work hero={rp3Hero}/>
-            {/* </Route> */}
-            {/* <Route exact path="/work/abigails-driving-school"> */}
+            </Route>
+            <Route exact path="/work/abigails-driving-school">
               <Work hero={adsHero}/>
-            {/* </Route> */}
-            {/* <Route exact path="/work/noise-generator"> */}
+            </Route>
+            <Route exact path="/work/noise-generator">
               <Work hero={noiseHero}/>
-            {/* </Route> */}
+            </Route>
         <Footer/>
       </div>
-
+    </Router>
   );
 }
 

d0a31eef77bd3d6b998d2f6101b7407c03d20311
Nathan Keeyss <keeysnc@gmail.com>
2020-10-22 01:08:09 -0400

    Updates

---
+++ precache-manifest.c159093f8dfab3a1810daa88a8e80b7e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "762734dbc2183126220c91a837271626",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b57b4ffe7a5eaa3b121",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d012a8c799e2fe17cb63",
+    "url": "/static/css/main.512a708e.chunk.css"
+  },
+  {
+    "revision": "4b57b4ffe7a5eaa3b121",
+    "url": "/static/js/2.63623353.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.63623353.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d012a8c799e2fe17cb63",
+    "url": "/static/js/main.ddc533e6.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "f8142e350098aefe79ce30c077ee259a",
+    "url": "/static/media/80percent-logo.f8142e35.svg"
+  },
+  {
+    "revision": "1eca0443adaa36a64deb443d54de7bfd",
+    "url": "/static/media/about-logo-alt.1eca0443.svg"
+  },
+  {
+    "revision": "45f740898953090e27b6b1beffcf8988",
+    "url": "/static/media/about-pic.45f74089.png"
+  },
+  {
+    "revision": "fa51026cc879f9069a004f73f1f22e66",
+    "url": "/static/media/codepen-alt.fa51026c.svg"
+  },
+  {
+    "revision": "84a3a2c0ded3dab194344290d35fcf08",
+    "url": "/static/media/lwd-logo.84a3a2c0.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1b961ec67c1a48941e15255d44108fa9.js"
+  "/precache-manifest.c159093f8dfab3a1810daa88a8e80b7e.js"
 );
 
 self.addEventListener('message', (event) => {

4b898476c7715a0394b5dc3292b85d2ddb3685e4
Nathan Keeys <nathankeeys@Nathans-MacBook-Pro.local>
2021-04-30 14:24:46 -0400

    content update

--- src/components/About.js
+++ src/components/About.js
@@ -13,8 +13,9 @@ function About() {
         <div className="about">
             <h1 className="title">About Me</h1>
             <p style={{lineHeight: '1.7rem'}}>
-            As an artist and technologist, I'm intrigued about new technologies and the ways humans interact with them. 
-            Creating interactive art is an outlet for myself as well as an exciting way to make small experiences for others to enjoy.
+            As an designer and creative technologist, I'm intrigued about new technologies and the ways humans interact with them. 
+            I specialize in modern technology used to design & develop web interfaces as well as interactive web experiences.
+            I've been fortunate to work in the creative industry for 8+ years working with clients such as Giant, MGM National Harbor, Accenture, Childrens Hospital and more. 
             </p>
         </div>
         </div>
--- src/components/Bio.js
+++ src/components/Bio.js
@@ -8,32 +8,32 @@ function Bio() {
     return (
       <div className="section">
         <div className="section">
-        <h1 className="title">A collection of audio and visual projects by Nathan Keeys</h1>
+        <h1 className="title">My name is Nathan Keeys - Creative Technologist and founder of UX studio, Digital Anthro.</h1>
         <h4 className="subtitle"></h4>
-          <p style={{lineHeight: '1.7rem'}}>
-          This website showcases a mix of self-initiated and collaborative audio and visual projects.
-          I have a passion for creating interactive art and experimental music using new technologies. Feel free to explore, listen and view these works. 
-          </p>
+          <h4 style={{lineHeight: '1.7rem'}}>
+          This website showcases a mix of self-initiated projects, client work and collaborative interactive projects. Feel free to explore, listen and view these works. 
+          </h4>
         </div>
-        <h1>Audio & Visual Projects</h1>
-        <br/>
-        <h4><a target="_blank" href="http://noisegen.io/">Noisegen.io - Experimental Perlin Noise Generator</a></h4>
+        <h1>Work</h1>
+        <h4><a target="_blank" href="http://noisegen.io/">Noisegen.io - Design, WebGL and Three.js Development</a></h4>
+        <h4><a target="_blank" href="https://abigailsdrivingschool.com/en/">Abigails Driving School - Gatsby.js Development</a></h4>
+        <h4><a target="_blank" href="https://www.vinvox.com/">Vinvox - SVG Web Animations</a></h4>
+        <h4><a target="_blank" href="https://carpedmdating.com/">CarpeDM Dating - Wordpress Development</a></h4>
         <h4><a target="_blank" href="https://80cacao.bandcamp.com/">80% Cacao - Electronic Music Project</a></h4>
-        {/* <br/>
+        <br/>
         <br/>
         <h1>Talks & Teaching</h1>
+        <h4><a target="_blank" href="https://vimeo.com/463281152">Introduction to Node.js Review</a></h4> 
         <br/>
-        <h4><a target="_blank" href="https://vimeo.com/463281152">Introduction to Node.js Review</a></h4> */}
         <br/>
-        <br/>
-        {/* <h1>Awards</h1>
-        <br/>
+        <h1>Awards</h1>
         <h4><a href="https://www.awwwards.com/sites/noisegen-io">Awwwards - Honorable Mention - noisegen.io</a></h4>
         <h4><a href="https://www.awwwards.com/sites/noisegen-io">Awwwards - Mobile Excellence - noisegen.io</a></h4>
-        <h4><a target="_blank" href="https://borensteingroup.com/tate-tryon-top-non-profit-accounting-firm-mints-gold-best-professional-services-website-2017-annual-international-davey-awards/">Clutch - Best Professional Service Website 2017</a></h4> */} 
-        <h1>Exhibits</h1>
+        <h4><a target="_blank" href="https://borensteingroup.com/tate-tryon-top-non-profit-accounting-firm-mints-gold-best-professional-services-website-2017-annual-international-davey-awards/">Clutch - Best Professional Service Website 2017</a></h4>  
         <br/>
-        <h4><a href="#">Noise Shift - Coming Soon</a></h4>
+        <br/>
+        <h1>Interactive Exhibits</h1>
+        <h4><a target="_blank" href="https://cicamuseum.com/over-the-structures-2021/">Noise Shift - CICA Museum - Over the Structures Exhibition 2021</a></h4>
       </div>
     );
   }
